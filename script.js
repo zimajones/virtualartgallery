620726dfd085660705b4c8c3b56c524e27a7631d
Jordan Jones <jordan.jones@mintellity.com>
2023-07-30 20:08:30 -0400

    have to put into flex; style button

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
-//this is for the intro text
+//this is for the intro text on home screen
 const introText = document.getElementById('intro-h1');
 const secondText = document.getElementById('second-h1');
-
-
-
+const introButton = document.getElementById('enter-button');
 
 setTimeout(() => {
-    
     introText.classList.remove('hide');
     introText.classList.add('fadeIn');
-
-
-    
-
-}, 2000);
+}, 1000);
 
 setTimeout(() => {
-    
     secondText.classList.remove('hide');
     secondText.classList.add('fadeIn');
+}, 3000);
 
-
-    
-
-}, 4000);
-
+setTimeout(() => {
+    introButton.classList.remove('hide');
+    introButton.classList.add('fadeIn');
+}, 5000);
 
 
 

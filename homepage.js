864b5f59338f9b5dc039641519b691acc0e623d4
Jordan Jones <jordan.jones@mintellity.com>
2023-08-02 15:12:47 -0400

    have carousel; use grid

--- homepage.js
+++ homepage.js
@@ -4,3 +4,20 @@ const navbarLinks = document.getElementsByClassName('nav-links')[0];
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
 })
+
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
+buttons.forEach(button => (
+    button.addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === 'next' ? 1 : -1;
+        const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
+
+        const activeSlide = slides.querySelector("[data-active]")
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+        if(newIndex < 0) newIndex = slides.children.length - 1;
+        if(newIndex >= slides.children.length) newIndex = 0;
+        
+        slides.children[newIndex].dataset.active = true;
+        delete activeSlide.dataset.active;
+    })
+))
